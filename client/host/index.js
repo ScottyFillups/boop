b42334093961619753338d4ccb68474a7f671a84
Philip Scott <pscott@zeptohost.com>
2018-03-05 11:15:21 -0800

    Revert "Add properly locking when game starts"

--- client/host/index.js
+++ client/host/index.js
@@ -239,6 +239,5 @@ $('#play-game').addEventListener('submit', (e) => {
   e.preventDefault()
   document.body.appendChild(renderer.domElement)
   $('#dom').classList.add('hide')
-  socket.emit('lock')
   startGame()
 })
